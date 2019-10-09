f334138053fd0959e4a405393d1a28c9085586a8
karsti11 <53300206+karsti11@users.noreply.github.com>
2019-10-09 12:22:25 +0200

    Delete feature_spectral_flux.m

--- feature_spectral_flux.m
+++
@@ -1,8 +0,0 @@
-function F = feature_spectral_flux(windowFFT, windowFFTPrev)
-
-%normalize the two spectra
-windowFFT = windowFFT / sum(windowFFT);
-windowFFTPrev = windowFFTPrev / sum(windowFFTPrev + eps);
-
-%compute the spectral flux as the sum of square distances
-F = sum((windowFFT - windowFFTPrev) .^ 2);
